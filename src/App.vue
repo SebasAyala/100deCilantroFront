27d9944cb0a114f0493132b499d719556dc3e929
programador_ori <programador.ori@udi.edu.co>
2020-02-22 12:13:47 -0500

    Primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      height="84"
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="images/logo-normal.png"
+          transition="scale-transition"
+          width="200"
+        />
+      </div>
+
+        <v-text-field
+          class="ml-10 mr-10 center mt-8"
+          label="Buscar película"
+          prepend-inner-icon="mdi-magnify"
+          outlined
+          clearable
+        />
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Login</span>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+      <Movie/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
+import Movie from './components/Movie';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+    Movie,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Movie.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-container>
+
+        <v-card
+            :loading="loading"
+            class="mb-5"
+            max-width="260"
+        >
+            <v-img
+            height="350"
+            src="https://i.pinimg.com/originals/92/c8/e0/92c8e00b34fcfdeaf605a0647c21adb3.jpg"
+            ></v-img>
+
+            <v-card-title class="py-2">Avengers End-Game</v-card-title>
+
+            <v-card-text class="pb-0">
+            <v-row
+                align="center"
+                class="mx-0"
+            >
+                <v-rating
+                :value="4.5"
+                color="amber"
+                dense
+                half-increments
+                readonly
+                size="14"
+                ></v-rating>
+
+                <div class="grey--text ml-4">4.5 (413)</div>
+            </v-row>
+            <v-chip-group
+                class="pt-3"
+                v-model="selection"
+                active-class="deep-purple accent-4 white--text"
+                column
+            >
+                <v-chip small>5:30PM</v-chip>
+
+                <v-chip small>7:30PM</v-chip>
+
+                <v-chip small>8:00PM</v-chip>
+            </v-chip-group>
+            </v-card-text>
+
+            <v-card-actions>
+            <v-btn
+                color="green accent-3"
+            >
+                Reservar
+            </v-btn>
+            </v-card-actions>
+        </v-card>
+
+    </v-container>
+</template>
